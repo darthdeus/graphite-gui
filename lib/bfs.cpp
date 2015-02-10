068e2c61470b7de4fac9c055fcdddbc22c99d265
Jakub Arnold <darthdeus@gmail.com>
2015-02-10 10:15:13 +0100

    Minor cleanup

--- lib/bfs.cpp
+++ lib/bfs.cpp
@@ -22,18 +22,6 @@ struct color
     explicit color(vertex_color c) : c(c) {}
 };
 
-//static color white(vertex_color::white);
-//static color gray(vertex_color::gray);
-//static color black(vertex_color::black);
-
-//static void color_white(Vertex *v) { v->metadata = &white; }
-//static void color_gray(Vertex *v) { v->metadata = &gray; }
-//static void color_black(Vertex *v) { v->metadata = &black; }
-
-//static bool is_white(Vertex *v) { return ((color *)v->metadata) == &white; }
-//// static bool is_gray(vertex* v) { return ((color*)v->metadata) == &gray; }
-//static bool is_black(Vertex *v) { return ((color *)v->metadata) == &black; }
-
 BFS::BFS(Graph &g, Vertex* s, Vertex* e) : g(g), start_(s), end_(e) {
     start();
 }
